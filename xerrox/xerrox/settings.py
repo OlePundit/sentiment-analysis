f30b669a9c5ed8b2fa5ad9ae604a57c1414bf5d7
OlePundit <glennomondi@yahoo.com>
2022-11-05 09:34:10 -0700

    db settings

--- xerrox/xerrox/settings.py
+++ xerrox/xerrox/settings.py
@@ -31,7 +31,7 @@ ACCESS_TOKEN_SECRET = '9cMoUJUMyoIxh4cLozkWLGtfEjcDm6mc70WxbUPvU17c9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['67.205.168.227', 'm.x-planetory.com']
+ALLOWED_HOSTS = ['67.205.168.227', 'm.x-planetory.com', 'localhost']
 
 
 # Application definition
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'xerrox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASES= {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'brandsent',
+        'USER' : 'glenn',
+        'PASSWORD' : 'Glennisgreat14$',
+        'HOST' : 'localhost',
+        'PORT' : '',
     }
 }
 
